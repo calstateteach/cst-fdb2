88a74e92ab3046a9ddf6fde4953203db9592ad56
Terence Shek <tshek@calstateteach.net>
2018-06-07 15:46:13 -0700

    Change pubic api for assignment navigation to use dashboard list of assignments.

--- routes/api_public_0/apiPublicRoutes.js
+++ routes/api_public_0/apiPublicRoutes.js
@@ -6,6 +6,7 @@ This API provides unsecured access to Canvas assignment data for cross-domain AJ
 
 const express     = require('express');
 const router      = express.Router();
+const appConfig   = require('../../libs/appConfig');
 const canvasCache = require('../../libs/canvasCache');
 
 
@@ -25,10 +26,9 @@ function assignmentNavigationHandler(req, res) {
   let courseId = parseInt(req.params['courseId'], 10);
   let assignmentId = parseInt(req.params['assignmentId'], 10);
 
-  // Allow cross origin request
+  // Allow cross origin request so Canvas Web page to call this with AJAX.
   res.set('Access-Control-Allow-Origin', '*');
   // res.set('Access-Control-Allow-Origin', 'https://calstateteach.test.instructure.com');
-
 
   // If no data found for the parameters, that's an error.
   function noDataErr() {
@@ -36,27 +36,59 @@ function assignmentNavigationHandler(req, res) {
     return res.json({ err: err});
   }
 
-  const assignments = canvasCache.getCourseAssignments(courseId);
-  if (assignments) {
-    const assignmentIndex = assignments.findIndex( e => e.id === assignmentId);
-    if (assignmentIndex < 0) {
-      return noDataErr();
-    } else {
-      // Calculate prev & next assignments in the list. Navigation is circular.
-      const prevId 
-        = (assignmentIndex === 0) ? assignments[assignments.length - 1].id : assignments[assignmentIndex - 1].id;
-      const nextId
-        = (assignmentIndex === (assignments.length - 1)) ? assignments[0].id : assignments[assignmentIndex + 1].id;
-    
-      const nav = {
-        prev: prevId,
-        next: nextId
-      }
-      return res.json(nav);
-    } // end else assignment navigation to figure out
-  } else {  // If no data found for the parameters, that's an error.
-    return noDataErr();
+  // Use the same set of assignments as the faculty dashboard
+  const terms = appConfig.getTerms();
+  const courseConfig = terms.find( e => e.course_id === courseId);
+  if (!courseConfig) return noDataErr();
+
+  const modules = canvasCache.getCourseModules(courseId);
+  if (!modules) return noDataErr();
+
+  var assignmentIds = [];   // Accumulate ordered list of assignments as used by the dashboard
+
+  for (let moduleIndex of courseConfig.module_indices) {
+    const assignments = modules[moduleIndex].items.filter( e => e.type === 'Assignment');
+    assignmentIds = assignmentIds.concat(assignments.map( e => e.content_id));
   }
+
+  const assignmentIndex = assignmentIds.findIndex( e => e === assignmentId);
+  if (assignmentIndex < 0) {
+    return noDataErr();
+  } else {
+    // Calculate prev & next assignments in the list. Navigation is circular.
+    const prevId 
+      = (assignmentIndex === 0) ? assignmentIds[assignmentIds.length - 1] : assignmentIds[assignmentIndex - 1];
+    const nextId
+      = (assignmentIndex === (assignmentIds.length - 1)) ? assignmentIds[0] : assignmentIds[assignmentIndex + 1];
+  
+    const nav = {
+      prev: prevId,
+      next: nextId
+    }
+    return res.json(nav);
+  }
+
+  // const assignments = canvasCache.getCourseAssignments(courseId);
+  // if (assignments) {
+  //   const assignmentIndex = assignments.findIndex( e => e.id === assignmentId);
+  //   if (assignmentIndex < 0) {
+  //     return noDataErr();
+  //   } else {
+  //     // Calculate prev & next assignments in the list. Navigation is circular.
+  //     const prevId 
+  //       = (assignmentIndex === 0) ? assignments[assignments.length - 1].id : assignments[assignmentIndex - 1].id;
+  //     const nextId
+  //       = (assignmentIndex === (assignments.length - 1)) ? assignments[0].id : assignments[assignmentIndex + 1].id;
+    
+  //     const nav = {
+  //       prev: prevId,
+  //       next: nextId
+  //     }
+  //     return res.json(nav);
+  //   } // end else assignment navigation to figure out
+  // } else {  // If no data found for the parameters, that's an error.
+  //   return noDataErr();
+  // }
 }
 
 
