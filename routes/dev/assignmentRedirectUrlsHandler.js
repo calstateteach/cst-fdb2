046a9db6da0173cf57d21a40cfa58ff996ba3935
Terence Shek <tshek@calstateteach.net>
2018-06-29 15:26:53 -0700

    Version for public github repo

--- routes/dev/assignmentRedirectUrlsHandler.js
+++ routes/dev/assignmentRedirectUrlsHandler.js
@@ -12,14 +12,6 @@ const canvasCache = require('../../libs/canvasCache');
 
 function get(req, res) {
 
-  // List of term courses
-  // const terms = req.app.locals.moduleMap.json.map( e => {
-  //   return {
-  //     name: e.name,
-  //     courseId: e.course_id
-  //   };
-  // });
-
   // Collect all assignment URLs for the term courses into an object.
   // Each element of the collection is an object representing one
   // assignment, identified by its name.
@@ -71,55 +63,6 @@ function get(req, res) {
   };
   return res.render('dev/assignmentRedirectUrls', params);
 }
-
-  
-
-//   function iteratee(courseId, callback) {
-//     // callback signature: (err)
-//     canvasCache.getCourseAssignments(req, courseId, true, (err, json) => {
-//       if (err) return callback(err);
-
-//       // We're just interested in the assignment name and URL
-//       const assignments = json.map( e => {
-//         return {
-//           name: e.name,
-//           url: e.html_url
-//         };
-//       });
-
-//       for( let assignment of assignments) {
-//         // See if we've already stashed an assignment object for this assignment name
-//         var stashedAssignment = courseAssignments.find( e => e.name === assignment.name);
-
-//         // If we haven't an object with this assignment name yet, make one and stash it.
-//         if (!stashedAssignment) {
-//           stashedAssignment = {
-//             name: assignment.name,
-//             url: {}
-//           };
-//           courseAssignments.push(stashedAssignment);
-//         }
-
-//         // Add the course's assignment URL to the collection
-//         stashedAssignment.url[courseId] = assignment.url;
-//       } // end loop through course's assignments
-      
-//       return callback();
-//     }); // end canvas cache callback
-//   } // end async iteratee function definition
-
-//   function doneIterating(err) {
-//     if (err) return res.render('dev/err', { err: err });
-//     const params = {
-//       terms: terms,
-//       courseAssignments: courseAssignments
-//     };
-//     return res.render('dev/assignmentUrls', params);
-//   }
-
-//   async.eachSeries(terms.map( e => e.courseId), iteratee, doneIterating);
-// }
-
 
 //******************** Exports ********************//
 
